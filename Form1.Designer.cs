cb5835ec09ac569fab9ef5ec29f2b386bed4f776
dmitry2k <dmitry.lyudovskikh@gmail.com>
2016-06-25 14:49:01 +0300

    first version

---
+++ Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Gr2_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(43, 32);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(900, 675);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(-2, -2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(897, 670);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "&Load";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(82, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "&Save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.DefaultExt = "png";
+            this.openFileDialog1.Filter = "Image files (*.bmp, *.jpg, *.png, *.gif)|*.bmp,;*.jpg;*.png;*.gif";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.DefaultExt = "png";
+            this.saveFileDialog1.Filter = "PNG-files (*.png) | *.png";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(163, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(264, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Применить фильтр ко всему изображению";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(433, 3);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(134, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Удалить фильтр";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(632, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(220, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Можно выделять часть картинки мышкой";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 712);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Gr2_3";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Gr2_2
+{
+    public partial class Form1 : Form
+    {
+
+        private Image image,image1;
+        private Point start, finish;
+        private Rectangle rectangle;
+        private bool f = false;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            openFileDialog1.InitialDirectory = saveFileDialog1.InitialDirectory = Directory.GetCurrentDirectory();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string s = openFileDialog1.FileName;
+                try
+                {
+                    Image im = new Bitmap(s);
+                    if (pictureBox1.Image != null)
+                        pictureBox1.Image.Dispose();
+                    pictureBox1.Image = im;
+                    image = im;
+                    image1 = im;
+                }
+                catch
+                {
+                    MessageBox.Show("File " + s + "has a wrong format.", "Error");
+                    return;
+                }
+                openFileDialog1.FileName = "";
+            }
+            f = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string s0 = saveFileDialog1.FileName;
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string s = saveFileDialog1.FileName;
+                if (s.ToUpper() == s0.ToUpper())
+                {
+                    s0 = Path.GetDirectoryName(s0) + "\\($$##$$).png";
+                    pictureBox1.Image.Save(s0);
+                    pictureBox1.Image.Dispose();
+                    File.Delete(s);
+                    File.Move(s0, s);
+                    pictureBox1.Image = new Bitmap(s);
+                }
+                else
+                    pictureBox1.Image.Save(s);
+            }
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            rectangle = new Rectangle(0, 0, pictureBox1.Image.Width, pictureBox1.Image.Height);
+            Random rnd = new Random();
+            Point[] points = new Point[Math.Max(rectangle.Width,rectangle.Height)];
+            for (int i = 0; i < points.GetLength(0); ++i)
+                points[i] = new Point(rnd.Next(rectangle.Width), rnd.Next(rectangle.Height));
+            image = Voronoi(new Bitmap(pictureBox1.Image), rectangle, points);
+            pictureBox1.Image = image;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = image1;
+            image = image1;
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (f)
+                f = false;
+            else
+                start = e.Location;
+        }
+
+        private class PointComparer : IComparer<Point>
+        {
+            public int Compare(Point p1, Point p2)
+            {
+                if (p1.Y < p2.Y)
+                    return -1;
+                else
+                    if (p1.Y > p2.Y)
+                        return 1;
+                    else
+                        if (p1.X < p2.X)
+                            return -1;
+                        else
+                            if (p1.X > p2.Y)
+                                return 1;
+                            else
+                                return 0;
+            }
+        }
+
+        private double distance(int x1, int y1, int x2, int y2)
+        {
+            return (Math.Sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)));
+        }
+
+        private Bitmap Voronoi(Bitmap bm, Rectangle rect, Point[] points)
+        {
+            int[,] area_property = new int[points.GetLength(0), 4];
+            Color[] area_colors = new Color[points.GetLength(0)];
+            int[,] im_points = new int[rect.Width, rect.Height];
+
+            for (int i = 0; i < rect.Width; ++i)
+                for (int j = 0; j < rect.Height; ++j)
+                {
+                    double min_d = distance(i, j, points[0].X, points[0].Y);
+                    im_points[i, j] = 0;
+                    for (int k = 1; k < points.GetLength(0); ++k)
+                    {
+                        double d = distance(i, j, points[k].X, points[k].Y);
+                        if (min_d > d)
+                        {
+                            min_d = d;
+                            im_points[i, j] = k;      // принадлежит области точки k
+                        }
+                    }
+                    area_property[im_points[i, j], 0] += bm.GetPixel(i + rect.Left, j + rect.Top).R;  //сумма всех красных компонент точек области
+                    area_property[im_points[i, j], 1] += bm.GetPixel(i + rect.Left, j + rect.Top).G;   //сумма всех зеленных компонент точек области
+                    area_property[im_points[i, j], 2] += bm.GetPixel(i + rect.Left, j + rect.Top).B;  //сумма всех синих компонент точек области
+                    ++area_property[im_points[i, j], 3];  //количество точек в области точки
+                }
+            
+            for (int k = 0; k < points.GetLength(0); ++k)
+            {
+                if ((double)area_property[k, 3] == 0)
+                    continue;
+                int r = (int)Math.Round((double)area_property[k, 0] / (double)area_property[k, 3]);  //среднее всех красных компонент точек области
+                int g = (int)Math.Round((double)area_property[k, 1] / (double)area_property[k, 3]);  //среднее всех зеленных компонент точек области
+                int b = (int)Math.Round((double)area_property[k, 2] / (double)area_property[k, 3]);  //среднее всех синих компонент точек области
+                area_colors[k] = Color.FromArgb(r, g, b); //цвет области
+            
+            }
+
+            //красим области
+            for (int i = 0; i < rect.Width; ++i)
+                for (int j = 0; j < rect.Height; ++j)
+                    bm.SetPixel(rect.Left + i, rect.Top + j, area_colors[im_points[i, j]]);
+
+            //разделяем области
+            for (int i = rect.Left; i < rect.Right-1; ++i)
+                for (int j = rect.Top; j < rect.Bottom - 1; ++j)
+                    if (bm.GetPixel(i, j) != bm.GetPixel(i, j + 1) || bm.GetPixel(i, j) != bm.GetPixel(i + 1, j))
+                        bm.SetPixel(i, j, Color.White);
+
+            return bm;
+        }
+
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (f)
+                f = false;
+            else
+                if (e.Button == MouseButtons.Left)
+                {
+                    finish = e.Location;
+                    Bitmap bm = new Bitmap(image);
+                    Graphics g = Graphics.FromImage(bm);
+                    rectangle = new Rectangle(Math.Min(start.X, finish.X), Math.Min(start.Y, finish.Y), Math.Abs(start.X - finish.X), Math.Abs(start.Y - finish.Y));
+                    g.DrawRectangle(Pens.Black, rectangle);
+                    pictureBox1.Image = bm;
+                }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+
+            pictureBox1.Image = image;
+            Random rnd = new Random();
+            Point[] points = new Point[Math.Max(rectangle.Width,rectangle.Height)];
+            for (int i = 0; i < points.GetLength(0); ++i)
+                points[i] = new Point(rnd.Next(rectangle.Width), rnd.Next(rectangle.Height));
+            image = Voronoi(new Bitmap(pictureBox1.Image), rectangle, points);
+            pictureBox1.Image = image;          
+        }
+
+    }
+}
